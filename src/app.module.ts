ec41067a4acf3b02890093359939c4bfa15e5d10
Jacek Skrzypek <devontheroof@gmail.com>
2023-11-19 17:51:46 +0100

    install and import mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductsModule, ScheduleModule.forRoot()],
+  imports: [
+    ProductsModule,
+    ScheduleModule.forRoot(),
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+  ],
 })
 export class AppModule {}
