223c9343521b676516db2be9ff026438a22819cd
Jacek Skrzypek <devontheroof@gmail.com>
2023-11-24 16:37:01 +0100

    delete old imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,12 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { MongooseModule } from '@nestjs/mongoose';
-import { VendorsModule } from './vendors/vendors.module';
-import { ManufacturersModule } from './manufacturers/manufacturers.module';
 
 @Module({
   imports: [
     ProductsModule,
     ScheduleModule.forRoot(),
     MongooseModule.forRoot('mongodb://localhost/nest'),
-    VendorsModule,
-    ManufacturersModule,
   ],
 })
 export class AppModule {}
